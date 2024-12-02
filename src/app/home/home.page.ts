1e7ac9dba6d29e75db042fd6a94a64922f7b83d7
Kishan_maurya <kishan.maurya@cashfree.com>
2024-12-02 16:48:58 +0530

    Sample code Ionic-Capacitor with Angular

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {CFEnvironment, CFPaymentGateway, CFSession, CFWebCheckoutPayment} from "@awesome-cordova-plugins/cashfree-pg";
+
 
 @Component({
   selector: 'app-home',
@@ -10,4 +12,23 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
 })
 export class HomePage {
   constructor() {}
+  async initiateWebPayment(){
+    const callbacks = {
+        onVerify: function (result:any) {
+            console.log("This is in the SDK Verify: " + JSON.stringify(result));
+        },
+        onError: function (error: any) {
+            console.log("This is in the SDK Error: " + JSON.stringify(error));
+        }
+    };
+    CFPaymentGateway.setCallback(callbacks)
+    CFPaymentGateway.doWebCheckoutPayment(
+        new CFWebCheckoutPayment(
+            new CFSession("session_Z7Sj6ZyrTNiU1PWxrgPoQ3zNkbQpPrSAhiilBn6mZnOq5PvIT7NqVnw9vm-6Wb5cT3HBTqufejP-UlK6OEiI88fYXvEYIadw6vEp_TLbGgXc",
+                "order_101024392penKNwgrYS4EcTwEWHIFlCdApr",
+                CFEnvironment.SANDBOX
+            ),
+            null)
+    )
+  };
 }
